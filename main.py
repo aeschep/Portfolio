c38715c863a1d553ada26f2ef34dc9e1f637ce87
Anna Scheppele <aeschep@umich.edu>
2016-04-01 16:52:49 -0400

    Finished sign up/log in (I think)

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         #saving the path/page name as a variable
         url = self.request.path
+        logging.info(url)
         #open the page with the url or open home page if that fails
         try:
             template = JINJA_ENVIRONMENT.get_template('templates' + url)
@@ -39,26 +40,40 @@ class FormHandler(webapp2.RequestHandler):
 
     def post(self):
         #post method for when the form is submitted
+        url = self.request.path
         #saving the username and password as variables
         email = self.request.get("email1")
         pw = self.request.get("pw1")
         #dictionary to hold email and password data
         login_data = {"test@test.com":"testpass"}
-
-        if (email in login_data) and (login_data[email] == pw):
-            logging.info("successful login")
-            template = JINJA_ENVIRONMENT.get_template('templates/loggedin.html')
-            self.response.write(template.render())
+        #if they are trying to log in
+        if  (url =="/login.html"):
+            #if email and password are correct, login
+            if (email in login_data) and (login_data[email] == pw):
+                logging.info("successful login")
+                template = JINJA_ENVIRONMENT.get_template('templates/loggedin.html')
+                self.response.write(template.render())
+            #if password is incorrect
+            elif (email in login_data) and (login_data[email] != pw):
+                template = JINJA_ENVIRONMENT.get_template('templates/login.html')
+                self.response.write(template.render({"fail": "Incorrect password, try again."}))
+            #if email is not found
+            elif (email not in login_data):
+                template = JINJA_ENVIRONMENT.get_template('templates/login.html')
+                self.response.write(template.render({"fail": "Incorrect email, try again."}))
+        #if none of above, they are signing up
         else:
+            #save the email and password in the dictionary
             login_data[email] = pw
             logging.info("saving email " + email + " and password " + pw)
 
+            #sending an email confirming signing up to the user
             sender_email = "templates-1211@appname.appspotmail.com"
             subject = "Thank you for signing up on Anna Scheppele's Portfolio!"
             body = """Thank you for signing up and making an account! 
             There is no extra functionality for creating an account on my website, but it allows me to practice sending emails!"""
-            mail.send_mail(sender_address, email, subject, body)
-
+            mail.send_mail(sender_email, email, subject, body)
+            #load the logged in page
             template = JINJA_ENVIRONMENT.get_template('templates/loggedin.html')
             self.response.write(template.render())
 
